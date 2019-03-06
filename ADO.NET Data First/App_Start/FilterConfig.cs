97354d33f90b52193937b402023c654a1f608534
Joshua Crocker <joshua+code@crocker.io>
2019-03-06 09:11:43 +0000

    📦 NEW: Add .gitignore

---
+++ ADO.NET Data First/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ADO.NET_Data_First
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ADO.NET Data First/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADO.NET_Data_First.MvcApplication" Language="C#" %>
